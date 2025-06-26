c8379d94780408ce776cf404ec135c4bfc32d76c
Julian Zanetti <julianzanetti99@gmail.com>
2025-06-25 21:43:57 -0300

    Finish Astro tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://julianzanetti.com',
+  site: 'https://julianzanetti.com',
+  integrations: [preact()],
 });
