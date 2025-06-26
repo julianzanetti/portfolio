26dc618504170a211d07532050b85f2eba217c8b
Julian Zanetti <julianzanetti99@gmail.com>
2025-06-26 14:41:02 -0300

    Se agrega tailwind.config.mjs y se modifica el diseño del Inicio (Hero.astro)

---
+++ tailwind.config.mjs
@@ -0,0 +1,11 @@
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#6c5ce7',
+      },
+    },
+  },
+  plugins: [],
+}
