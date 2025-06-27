aa0f550bc6391c03e806351ec258e6b8791ea7e4
Julian Zanetti <julianzanetti99@gmail.com>
2025-06-26 22:48:23 -0300

    Se agrega el footer a la web y se instala dependencias para los iconos

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,20 @@ import preact from '@astrojs/preact';
 
 import tailwind from '@astrojs/tailwind';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://julianzanetti.com',
-  integrations: [preact(), tailwind()],
+  integrations: [
+    preact({
+      exclude: ['**/SocialIcons.jsx'], // ⚠️ no usar React acá
+    }),
+    tailwind(),
+    react({
+      include: ['**/SocialIcons.jsx'], // ✅ solo usar React ahí
+    }),
+  ],
 
   vite: {},
 });
